4023175013a8eafce5b83bc71dcef54d24c55952
Kemi-Kostadinova <158486040+Kemi-Kostadinova@users.noreply.github.com>
2024-03-18 13:20:21 +0200

    Add footer component

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [NavigationComponent]
+  exports: [NavigationComponent, FooterComponent]
 })
 export class CoreModule { }
