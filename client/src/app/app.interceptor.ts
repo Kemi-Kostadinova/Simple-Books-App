f3be88e81f19f215c7a0d7fae914875ab9af722c
Kemi-Kostadinova <158486040+Kemi-Kostadinova@users.noreply.github.com>
2024-03-25 12:27:08 +0200

    Add app interceptor for authentication

---
+++ client/src/app/app.interceptor.ts
@@ -0,0 +1,38 @@
+import { Injectable, Provider } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+// import { ErrorService } from './core/error/error.service';
+import { Router } from '@angular/router';
+
+const { apiUrl } = environment
+
+@Injectable()
+class AppInterceptor implements HttpInterceptor {
+  API = '/api'
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    if (request.url.startsWith(this.API)) {
+      request = request.clone({
+        url: request.url.replace(this.API, apiUrl),
+        withCredentials: true
+      })
+    }
+    return next.handle(request)
+
+  }
+}
+
+export const appInterceptorProvider: Provider = {
+  useClass: AppInterceptor,
+  multi: true,
+  provide: HTTP_INTERCEPTORS
+}
