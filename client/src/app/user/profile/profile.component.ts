8669548c29f4564bf63eb7c745e48ebeb73b18e9
Kemi-Kostadinova <158486040+Kemi-Kostadinova@users.noreply.github.com>
2024-03-30 22:16:39 +0200

    Add profile page

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserForAuth } from 'src/types/user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  user: UserForAuth | undefined;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUser().subscribe(user => {
+      this.user = user;
+    })
+  }
 
 }
