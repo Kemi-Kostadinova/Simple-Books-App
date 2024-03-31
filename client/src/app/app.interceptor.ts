0b9b92073d9ca75d8ba05f9703d5664344cb9f4e
Kemi-Kostadinova <158486040+Kemi-Kostadinova@users.noreply.github.com>
2024-03-31 21:22:15 +0300

    Add error handling

--- client/src/app/app.interceptor.ts
+++ client/src/app/app.interceptor.ts
@@ -8,24 +8,33 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
+import { ErrorService } from './core/error/error.service';
+import { Router } from '@angular/router';
 
 const { apiUrl } = environment
 
 @Injectable()
 class AppInterceptor implements HttpInterceptor {
   API = '/api'
-  constructor() {}
+  constructor(private errorService: ErrorService, private router: Router) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
     if (request.url.startsWith(this.API)) {
-      
+
       request = request.clone({
         url: request.url.replace(this.API, apiUrl),
         withCredentials: true
       })
     }
-    return next.handle(request)
+
+    return next.handle(request).pipe(
+      catchError((err) => {
+        this.errorService.setError(err);
+        this.router.navigate(["/error"]);
+        return [err];
+      })
+    );
   }
 }
 
