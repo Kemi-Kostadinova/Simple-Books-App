6fd31342d20fe1ec84ec9263d8779bc5df039eee
Kemi-Kostadinova <158486040+Kemi-Kostadinova@users.noreply.github.com>
2024-04-02 18:49:20 +0300

    Add error handling on server

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,42 +1,68 @@
 const router = require("express").Router();
 
 const bookService = require("../services/bookService");
+const { getErrorMessage } = require("../utils/errorUtils");
 
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
+  try {
     const books = await bookService.getAll();
 
     res.json(books);
+  } catch (err) {
+    res.json({ error: getErrorMessage(err) });
+  }
 });
 
-router.post("/", async(req, res) => {
-    const bookData = req.body;
-    const newBookReview = await bookService.create({ ...bookData, owner: req.user?._id });
+router.post("/", async (req, res) => {
+  const bookData = req.body;
+
+  try {
+    const newBookReview = await bookService.create({
+      ...bookData,
+      owner: req.user?._id,
+    });
 
     res.json(newBookReview);
+  } catch (error) {
+    res.json({ error: getErrorMessage(err) });
+  }
 });
 
-router.get("/:bookId", async(req, res) => {
-    const bookId = req.params.bookId;
+router.get("/:bookId", async (req, res) => {
+  const bookId = req.params.bookId;
+
+  try {
     const book = await bookService.getOne(bookId);
-;
+
     res.json(book);
+  } catch (err) {
+    res.json({ error: getErrorMessage(err) });
+  }
 });
 
-router.put("/:bookId", async(req, res) => {
-    const bookId = req.params.bookId;
-    const currentBook = req.body;
+router.put("/:bookId", async (req, res) => {
+  const bookId = req.params.bookId;
+  const currentBook = req.body;
 
+  try {
     const book = await bookService.edit(bookId, currentBook);
 
     res.json(book);
+  } catch (err) {
+    res.json({ error: getErrorMessage(err) });
+  }
 });
 
-router.delete("/:bookId", async(req, res) => {
-    const bookId = req.params.bookId;
+router.delete("/:bookId", async (req, res) => {
+  const bookId = req.params.bookId;
 
+  try {
     await bookService.delte(bookId);
 
-    res.json({ok: true});
+    res.json({ ok: true });
+  } catch (err) {
+    res.json({ error: getErrorMessage(err) });
+  }
 });
 
 module.exports = router;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,45 +1,58 @@
 const router = require("express").Router();
 
 const userService = require("../services/userService");
+const { getErrorMessage } = require("../utils/errorUtils");
 
 router.post("/register", async (req, res) => {
-    const userData = req.body;
+  const userData = req.body;
 
+  try {
     const { userId, email, token } = await userService.register(userData);
 
-    res.cookie('auth', token);
+    res.cookie("auth", token);
 
     res.json({
-        userId,
-        email,
-        token,
-    })
+      userId,
+      email,
+      token,
+    });
+  } catch (err) {
+    res.json({ error: getErrorMessage(err) });
+  }
 });
 
 router.post("/login", async (req, res) => {
-    const userData = req.body;
+  const userData = req.body;
 
+  try {
     const { userId, email, token } = await userService.login(userData);
 
-    res.cookie('auth', token);
+    res.cookie("auth", token);
 
     res.json({
-        userId,
-        email,
-        token,
-    })
+      userId,
+      email,
+      token,
+    });
+  } catch (err) {
+    res.json({ error: getErrorMessage(err) });
+  }
 });
 
 router.get("/logout", async (req, res) => {
-    res.clearCookie("auth");
+  res.clearCookie("auth");
 
-    res.json({ok: true})
+  res.json({ ok: true });
 });
 
 router.get("/profile", async (req, res) => {
+  try {
     const currentUser = await userService.getInfo(req.user?._id);
 
     res.json(currentUser);
+  } catch (err) {
+    res.json({ error: getErrorMessage(err) });
+  }
 });
 
 module.exports = router;
